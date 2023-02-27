1ae3a8bd54c8d1446b93815242d32a03299e6f6c
Lashika Arunachalam <47450171+lashika-arunachalam@users.noreply.github.com>
2023-02-27 15:01:59 +0000

    Add files via upload

--- Welcome.py
+++ Welcome.py
@@ -8,7 +8,7 @@ st.markdown("<h1 style='color: #ed53ce; padding-top:0;'>Lashika Arunachalam</h1>
 st.markdown("<h3 style='padding-top:0;'>Data Science Intern @Innomatics Research Labs</h3>", unsafe_allow_html=True)
 
 
-image=Image.open('Pic.png')
+image=Image.open('pic.png')
 style = '<style>img{border: 2px solid None; padding: 10px; border-radius: 50%; margin-left: 550px; margin-top: 0px;}</style>'
 st.markdown(style, unsafe_allow_html=True)
 st.image(image, caption=None, width=300, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
